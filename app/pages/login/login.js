68923793846c9a5fb1ebfb7fe2301e0927a13036
Polo-10 <m10zajac@wp.pl>
2021-08-02 22:34:38 +0200

    fix
    This reverts commit fc4a38bd68d05ea37a9aaf5b24ebd710f773e7c6.

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 
+const SECRET_KEY =
+  "f51173edb96636075ae6ff028a25e647583cb8dc40cf38f78a7287d3987d6a78e8f5e940932d7933607ca0a1099e1faa97eff60e73a355b469fa62493ecb8e46";
+
 const LoginForm = () => {
   const [value, setValue] = useState({});
+  const [token, setToken] = useState({});
 
   const handleChangeValues = (e) => {
     setValue({ ...value, [e.target.name]: e.target.value });
@@ -22,13 +26,16 @@ const LoginForm = () => {
     axios
       .post("http://localhost:3000/api/login", data)
       .then((res) => {
-        localStorage.setItem("storedToken", res.data.token);
         console.log(res);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+  useEffect(() => {
+    localStorage.setItem("storedToken", SECRET_KEY);
+  });
 
   return (
     <form onSubmit={handleSubmit}>
