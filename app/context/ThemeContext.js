fa00ec22b13ba817de8ad628b27d6b868846240b
Bartłomiej Biernat <123biernat123@gmail.com>
2021-08-03 22:37:39 +0200

    Update ThemeContext.js

--- app/context/ThemeContext.js
+++ app/context/ThemeContext.js
@@ -3,19 +3,25 @@ import React, { useEffect, useState, createContext } from "react"
 export const ThemeContext = createContext()
 
 function useTheme() {
-  const storageName = "themeStockMarket"
-  const storageThemeState = localStorage.getItem(storageName)
-    ? localStorage.getItem(storageName)
-    : false
+  if (typeof window !== "undefined") {
+    const storageName = "themeStockMarket"
+    const storageThemeState = localStorage.getItem(storageName)
+      ? localStorage.getItem(storageName)
+      : false
 
-  const [theme, setTheme] = useState(storageThemeState == "true" ? true : false)
+    const [theme, setTheme] = useState(
+      storageThemeState == "true" ? true : false
+    )
 
-  const changeTheme = () => {
-    setTheme(!theme)
-    localStorage.setItem(storageName, !theme)
+    const changeTheme = () => {
+      setTheme(!theme)
+      localStorage.setItem(storageName, !theme)
+    }
+
+    return [theme, changeTheme]
+  } else {
+    return [null, null]
   }
-
-  return [theme, changeTheme]
 }
 
 export default function Provider({ children }) {
