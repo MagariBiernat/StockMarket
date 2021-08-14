f83374651739152bfe596e5c36d777e68e49273f
Bartłomiej Biernat <123biernat123@gmail.com>
2021-08-14 14:15:53 +0200

    Update 404.js

--- app/pages/404.js
+++ app/pages/404.js
@@ -1,13 +1,13 @@
 import React from "react"
-
+import Layout from "../components/Layout"
 const Custom404 = () => {
   return (
-    <div>
+    <Layout>
       <h1>Error 404 - Page Not Found</h1>
       <button onClick={() => (window.location.href = "/")}>
         Go back to main page
       </button>
-    </div>
+    </Layout>
   )
 }
 
