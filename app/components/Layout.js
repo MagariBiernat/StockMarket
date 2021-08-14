c658fbbeae2a9beb737d7d05b2a0080ea522a940
Bartłomiej Biernat <123biernat123@gmail.com>
2021-08-14 14:00:42 +0200

    GlobalStyle at Layout

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,23 +1,31 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 
 const Layout = ({ children, pageMeta }) => {
   return (
     <Wrapper>
+      <GlobalStyle />
       <Navbar />
       <main>{children}</main>
       <Footer />
     </Wrapper>
   )
 }
-
 const Wrapper = styled.div`
   position: relative;
   min-width: 100vw;
   min-height: 100vh;
   background-color: ${(props) => props.theme.bg};
+`
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    box-sizing:border-box;
+  }
 `
 
 export default Layout
